7b09f9b7734b56a6cb7a95e4fd0c619994a00e0f
김영빈 ( Kim youngbin) <114568768+ybin96@users.noreply.github.com>
2022-12-13 17:39:40 +0900

    spring security 연습

---
+++ securityTest2/src/test/java/com/example/demo/SecurityTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
