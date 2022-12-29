6770d8d48c464c4771fad4ef2a99f2e41606baf5
김영빈 ( Kim youngbin) <114568768+ybin96@users.noreply.github.com>
2022-12-29 17:05:54 +0900

    React Test

---
+++ reactTest/src/main/java/com/example/demo/NodeAjaxApplication.java
@@ -0,0 +1,13 @@
+	package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodeAjaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodeAjaxApplication.class, args);
+	}
+
+}
